9e355155b82497696d53625957d1bd7ca5ef0818
Evan Furlong <148148604+EvanFur@users.noreply.github.com>
2025-03-18 18:46:36 +0000

    added computer and human choice

--- javascript.js
+++ javascript.js
@@ -15,6 +15,39 @@ function getComputerChoice() {
     return computerChoice
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("What's your choice?").toLowerCase()
+
+    if (humanChoice === "rock") {
+        humanChoice = "rock"
+    } else if (humanChoice === "paper"){
+        humanChoice = "paper"
+    } else if (humanChoice === "scissors") {
+        humanChoice = "scissors"
+    }
+
+}
+
 let computerChoice = getComputerChoice()
-console.log(computerChoice)
+let humanChoice = getHumanChoice()
+let computerScore = 0
+let humanScore = 0
+
+if (humanChoice === computerChoice) {
+    print(Draw!)
+} else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore = 1+
+} else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore = 1+
+} else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore = 1+
+} else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore = 1+
+} else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore = 1+
+} else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore = 1+
+}
+
+
 
